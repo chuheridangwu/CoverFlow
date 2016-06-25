e37481334020272780d0bc3df9996238948b729b
董玉毛 <153100950@qq.com>
2016-06-25 13:56:05 +0800

    放到单个控制器，便于后期管理

---
+++ UIControllerView/HorizontalViewController.h
@@ -0,0 +1,13 @@
+//
+//  HorizontalViewController.h
+//  UIControllerView
+//
+//  Created by 龙少 on 16/6/25.
+//  Copyright © 2016年 龙少. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HorizontalViewController : UIViewController
+
+@end
---
+++ UIControllerView/HorizontalViewController.m
@@ -0,0 +1,37 @@
+//
+//  HorizontalViewController.m
+//  UIControllerView
+//
+//  Created by 龙少 on 16/6/25.
+//  Copyright © 2016年 龙少. All rights reserved.
+//
+
+#import "HorizontalViewController.h"
+
+@interface HorizontalViewController ()
+
+@end
+
+@implementation HorizontalViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
